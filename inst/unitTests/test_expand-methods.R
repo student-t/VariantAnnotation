43ada83b3ea17af33ff963231a8e7338defa4c9a
v.obenchain <v.obenchain@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-12-11 19:51:08 +0000

    fix expand() for the following cases (devel r97345): 1) a subset of INFO columns is selected 2) only one INFO column and it is selected 3) VCF has only one row
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_0/madman/Rpacks/VariantAnnotation@97569 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/test_expand-methods.R
+++ inst/unitTests/test_expand-methods.R
@@ -33,3 +33,21 @@ test_expand_structural <- function()
     exp <- expand(vcf)
     checkTrue(nrow(exp) == 8L)
 }
+
+test_expand_multiple_info <- function()
+{
+    fl <- system.file("unitTests", "cases", "multiple_INFO_fields.vcf", 
+                      package="VariantAnnotation")
+    vcf <- readVcf(fl, "GRCh37")
+    exp <- expand(vcf)
+    checkTrue(nrow(exp) == 3L)
+
+    ## single row, subset of INFO fields selected 
+    exp <- expand(vcf[1,])
+    checkTrue(nrow(exp) == 2L)
+
+    ## single row, single (only) INFO field selected
+    vcf <- readVcf(fl, "GRCh37", param=ScanVcfParam(info="AF"))
+    exp <- expand(vcf[1,])
+    checkTrue(nrow(exp) == 2L)
+}
